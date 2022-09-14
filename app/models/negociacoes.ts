dac672471bba781a96c5964caf86bdd5205c918e
Gabriel Godoy <gabe.profe@gmail.com>
2022-09-14 00:00:12 -0300

    refactor

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,19 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-  private negociacoes: Array<Negociacao> = [];
+  private negociacoes: Negociacao[] = [];
+
+  //Negociacao[] === Array<Negociacao>
+
 
   adiciona(negociacao: Negociacao){
     this.negociacoes.push(negociacao)
   }
 
-  lista(): ReadonlyArray<Negociacao> {
+  lista(): readonly Negociacao[] {
+
+  // lista(): ReadonlyArray<Negociacao> === lista(): readonly Negociacao[]
+    
     return this.negociacoes;
   }
 }
