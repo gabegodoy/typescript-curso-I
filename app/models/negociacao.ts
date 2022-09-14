2b3e5f1e04ad26617d78449e21a2a5fef810bcf6
Gabriel Godoy <gabe.profe@gmail.com>
2022-09-13 23:44:19 -0300

    constructor refactor

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,16 +1,10 @@
-import { Negociacoes } from "./negociacoes.js";
-
 export class Negociacao {
-    private _data :Date ;
-    private _quantidade :number;
-    private _valor :number;
-    private negociacoes = new Negociacoes;
-
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    
+    constructor(
+        private _data :Date ,
+        private _quantidade :number,
+        private _valor :number
+    ){}
 
     get data(): Date {
         return this._data;
