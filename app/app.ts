68e3d616310f8739197d4baccc1b0e1392a79838
Gabriel Godoy <gabe.profe@gmail.com>
2022-09-13 22:20:29 -0300

    params type && refactor

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,14 @@
+import { NegociacaoController } from './controllers/negociacao-controller.js';
 import { Negociacao } from './models/negociacao.js';
 
+const form = document.querySelector('.form');
+
 const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+
+form.addEventListener('submit', event => {
+
+  event.preventDefault();
+
+  controller.adiciona();
+})
