93e6b92673a422a91b483c2a5112212093f0e291
Gabriel Godoy <gabe.profe@gmail.com>
2022-09-23 03:06:57 -0300

    remove comments on compile

--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,12 +2,10 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
-    //Negociacao[] === Array<Negociacao>
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
     lista() {
-        // lista(): ReadonlyArray<Negociacao> === lista(): readonly Negociacao[]
         return this.negociacoes;
     }
 }
